f27d8eefeb1b3c8e7dc830cc2f77d3b8273b1bea
PC5-IFLAB <sylvana.freelance@gmail.com>
2023-03-21 16:33:57 +0700

    “menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,4 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace modul
