3f92568e413330b0fa513d7883886dad1bd1a5b3
PC5-IFLAB <sylvana.freelance@gmail.com>
2023-03-21 16:34:18 +0700

    “menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,4 +7,25 @@ using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace modul
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Penjumlahan penjumlahan = new Penjumlahan();
+            Console.WriteLine(Penjumlahan.JumlahTigaAngka<int>(13, 2, 21));
+        }
+    }
+
+
+    public class Penjumlahan
+    {
+        public T JumlahTigaAngka<T>(T lhs, T rhs, T xhs)
+        {
+            return (dynamic)lhs + (dynamic)rhs + (dynamic)rhs;
+        }
+
+    }
+
+}
